46cf47675bb7bdf034eb365a6155021a8f5cecea
Toad Wyrick <jow631s@gmail.com>
2021-02-14 14:45:01 -0600

    corrected table migrations, installed pg

--- migrations/20210206180015-create-users-bands-table.js
+++ migrations/20210206180015-create-users-bands-table.js
@@ -8,14 +8,14 @@ var seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
 //using migrate-up will create the following table
-exports.up = function(db) {
+exports.up = function (db) {
   return db.createTable('users_bands', {
     band_id: {
       type: 'int',
@@ -26,18 +26,18 @@ exports.up = function(db) {
         rules: {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-        },
-      },
-      user_id: {
-        type: 'int',
-        foreignKey: {
-          name: 'users_bands_user_id_fk',
-          table: 'users',
-          mapping: 'id',
-          rules: {
-            onDelete: 'CASCADE',
-            onUpdate: 'RESTRICT'
-          },
+        }
+      }
+    },
+    user_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'users_bands_user_id_fk',
+        table: 'users',
+        mapping: 'id',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
         }
       }
     }
@@ -45,7 +45,7 @@ exports.up = function(db) {
 };
 
 //migrate down will then delete that table
-exports.down = function(db) {
+exports.down = function (db) {
   return db.dropTable('users_bands');
 };
 
--- migrations/20210206180056-create-gigs-setlists-table.js
+++ migrations/20210206180056-create-gigs-setlists-table.js
@@ -8,14 +8,14 @@ var seed;
   * We receive the dbmigrate dependency from dbmigrate initially.
   * This enables us to not have to rely on NODE_PATH.
   */
-exports.setup = function(options, seedLink) {
+exports.setup = function (options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
   seed = seedLink;
 };
 
 //using migrate-up will create the following table
-exports.up = function(db) {
+exports.up = function (db) {
   return db.createTable('gigs_setlists', {
     gig_id: {
       type: 'int',
@@ -26,18 +26,18 @@ exports.up = function(db) {
         rules: {
           onDelete: 'CASCADE',
           onUpdate: 'RESTRICT'
-        },
-      },
-      setlist_id: {
-        type: 'int',
-        foreignKey: {
-          name: 'gigs_setlists_setlist_id_fk',
-          table: 'setlists',
-          mapping: 'id',
-          rules: {
-            onDelete: 'CASCADE',
-            onUpdate: 'RESTRICT'
-          },
+        }
+      }
+    },
+    setlist_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'gigs_setlists_setlist_id_fk',
+        table: 'setlists',
+        mapping: 'id',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
         }
       }
     }
@@ -45,7 +45,7 @@ exports.up = function(db) {
 };
 
 //migrate down will then delete that table
-exports.down = function(db) {
+exports.down = function (db) {
   return db.dropTable('gigs_setlists');
 };
 
